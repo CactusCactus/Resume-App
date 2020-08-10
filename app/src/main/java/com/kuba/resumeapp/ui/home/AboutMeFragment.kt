ff0b957c4f7880da6d46147b7802c0215e322bbc
jakub <jakub.grzeda95@gmail.com>
2020-08-10 03:12:59 +0200

    AboutMeFragment complete

--- app/src/main/java/com/kuba/resumeapp/ui/home/AboutMeFragment.kt
+++ app/src/main/java/com/kuba/resumeapp/ui/home/AboutMeFragment.kt
@@ -1,6 +1,8 @@
 package com.kuba.resumeapp.ui.home
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,11 +10,19 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.kuba.resumeapp.data.PersonalInfo
 import com.kuba.resumeapp.data.model.RequestStatus
 import com.kuba.resumeapp.databinding.FragmentAboutMeBinding
 import com.kuba.resumeapp.ui.BaseFragment
+import com.stfalcon.imageviewer.StfalconImageViewer
+import com.stfalcon.imageviewer.loader.ImageLoader
 
 
 class AboutMeFragment : BaseFragment() {
@@ -47,9 +57,25 @@ class AboutMeFragment : BaseFragment() {
     }
 
     private fun observeViewModel() {
+        viewModel.personalInfoLD.observe(viewLifecycleOwner, Observer { info ->
+            info?.let {
+                checkViewVisibility(it)
+            }
+        })
         viewModel.requestStatus.observe(viewLifecycleOwner, Observer { status ->
             binding.loadingView.visibility = if (status == RequestStatus.CALLING) VISIBLE else GONE
         })
+    }
+
+    private fun checkViewVisibility(info: PersonalInfo) {
+        binding.run {
+            phoneLayout.visibility = if (!info.phoneNumber.isNullOrBlank()) VISIBLE else GONE
+            phoneTitleTV.visibility = if (!info.phoneNumber.isNullOrBlank()) VISIBLE else GONE
+            emailLayout.visibility = if (!info.email.isNullOrBlank()) VISIBLE else GONE
+            emailTitleTV.visibility = if (!info.email.isNullOrBlank()) VISIBLE else GONE
+            linkedInLayout.visibility = if (!info.linkedInUrl.isNullOrBlank()) VISIBLE else GONE
+            linkedInTitleTV.visibility = if (!info.linkedInUrl.isNullOrBlank()) VISIBLE else GONE
+        }
     }
 
     fun onPhoneClick() {
@@ -64,8 +90,18 @@ class AboutMeFragment : BaseFragment() {
         resolveUri(viewModel.personalInfoLD.value?.linkedInUrl)
     }
 
+    fun onImageClick() {
+        viewModel.personalInfoLD.value?.imageUrl?.let { imageUrl ->
+            val imageArr = Array(1) { imageUrl }
+            StfalconImageViewer.Builder<String>(context, imageArr, ImageLoader { imageView, image ->
+                Glide.with(this).load(image).into(imageView)
+            }).show()
+        }
+
+    }
+
     private fun resolveUri(uriString: String?) {
-        uriString.let {
+        uriString?.let {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(it)
             val pm = activity?.packageManager
@@ -77,5 +113,17 @@ class AboutMeFragment : BaseFragment() {
 
 
     override fun hasActivityToolbar(): Boolean = false
+}
 
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide
+            .with(view)
+            .load(url)
+            .centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
+    }
 }
